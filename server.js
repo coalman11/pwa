db83023db5c80b0dd9877891577706a7dad3c0bb
Piyush Gupta <piyush.gupta23@infosys.com>
2019-07-19 14:10:28 +0530

    Trying Heroku deployment

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/*', (req,res) => {
     res.sendFile(path.json(__dirname,'dist/pwa-angualar/index.html'));
 })
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Server started");
 })
